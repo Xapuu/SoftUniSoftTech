311e757d1b949ba3030ed384f2a45810104caad4
Xapuu <xapu.ivanov@gmail.com>
2017-04-21 19:05:53 +0300

    Now it works

--- config/routes.js
+++ config/routes.js
@@ -9,7 +9,6 @@ const servicesController = require('./../controllers/services');
 const mailingController = require('./../controllers/meiling');
 const lettersProductionController = require('./../controllers/lettersProduction');
 const digitalPrintController = require('./../controllers/digitalPrint');
-<<<<<<< HEAD
 const cardsMailingController = require('./../controllers/cardsMailing');
 const transPrintController = require('./../controllers/transPrint');
 const booksPrintingController = require('./../controllers/booksPrinting');
@@ -24,17 +23,17 @@ const smsController = require('./../controllers/sms');
 const emailController = require('./../controllers/email');
 const elBooksController = require('./../controllers/elBooks');
 const solutionController = require('./../controllers/solution')
-=======
+
 
 var recaptcha = require('express-recaptcha');
 
 recaptcha.init('6Ld-rB0UAAAAANyXZKFiGbBc1J7NM_NQg4Y93xeF', '6Ld-rB0UAAAAANoVTHTNIkiX9p1H7HhqBlhQvWnE');
->>>>>>> 1f24866db8fd0377ea9a0a937e1381c331053b83
+
 
 module.exports = (app) => {
     app.get('/', homeController.index);
 
-<<<<<<< HEAD
+
     app.get('/solution/solution',solutionController.solution);
 
     app.get('/services/elBooks',elBooksController.elBooks);
@@ -61,11 +60,11 @@ module.exports = (app) => {
     app.get('/services/transPrint',transPrintController.transPrint);
 
     app.get('/services/cardsMailing',cardsMailingController.cardsMailing);
-=======
+
     // GitHEAD
     app.get('/user/register', recaptcha.middleware.render, userController.registerGet);
     app.post('/user/register', recaptcha.middleware.verify, userController.registerPost);
->>>>>>> 1f24866db8fd0377ea9a0a937e1381c331053b83
+
 
     app.get('/services/lettersProduction',lettersProductionController.lettersProduction);
 
--- models/Question.js
+++ models/Question.js
@@ -9,7 +9,7 @@ let questionSchema = mongoose.Schema({
     authorMail:{type:String, required:true},
     subject:{type:String, required:true},
     content:{type:String, required:true},
-    answeredBy:{type:String, default:""},
+    answeredBy:{type:ObjectId, required:true},
     answer:{type:String, default:""},
     dateAsked:{type:Date, default:Date.now()},
     answered: {type:Boolean, default:false}
